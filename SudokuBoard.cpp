e4a02ad4b725f5df6d5e26d79c5b684f9403f9aa
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2026-09-23 20:34:25 -0700

    Implement sudoku board utility functions

--- SudokuBoard.cpp
+++ SudokuBoard.cpp
@@ -15,3 +15,114 @@ void SudokuBoard::print() {
         }
     }
 }
+
+bool SudokuBoard::hasEmptyCells() {
+    return emptyCells().size() > 0;
+}
+
+bool SudokuBoard::isEmpty(const SudokuCell& cell)
+{
+    return 0 == board[cell.arrayIndex];
+}
+
+std::vector<SudokuCell> SudokuBoard::emptyCellsInRow(const int& row) {
+    std::vector<SudokuCell> cells{};
+    for (int col = 0;  col < 9; ++col)
+    {
+        const auto cell = SudokuCell(row, col);
+        if (isEmpty(cell))
+        {
+            cells.push_back(std::move(cell));
+        }
+    }
+    return cells;
+}
+
+std::vector<SudokuCell> SudokuBoard::emptyCellsInCol(const int& col) {
+    std::vector<SudokuCell> cells{};
+    for (int row = 0; row < 9; ++row)
+    {
+        const auto cell = SudokuCell(row, col);
+        if (isEmpty(cell))
+        {
+            cells.push_back(std::move(cell));
+        }
+    }
+    return cells;
+}
+
+std::vector<SudokuCell> SudokuBoard::emptyCellsInBlock(const int& block) {
+    std::vector<SudokuCell> cells{};
+    int startRow = (block / 3) * 3;
+    int startColumn = (block % 3) * 3;
+    for (int row = startRow; row < startRow + 3; ++row)
+    {
+        for (int col = startColumn; col < startColumn + 3; ++col)
+        {
+            const auto cell = SudokuCell(row, col);
+            if (isEmpty(cell))
+            {
+                cells.push_back(std::move(cell));
+            }
+        }
+    }
+    return cells;
+}
+
+std::vector<SudokuCell> SudokuBoard::emptyCells() {
+    std::vector<SudokuCell> emptyCells{};
+    for (int i = 0; i < static_cast<int>(board.size()); i++)
+    {
+        if (0 == board[i]) {
+            emptyCells.push_back(SudokuCell(i));
+        }
+    }
+
+    return emptyCells;
+}
+
+std::set<int> SudokuBoard::rowValues(const SudokuCell& cell) {
+    std::set<int> values{};
+
+    for (int col = 0; col < 9; ++col) {
+        if (auto value = board[cell.row() * 9 + col])
+        {
+            values.insert(board[value]);
+        }
+    }
+
+    return values;
+}
+
+std::set<int> SudokuBoard::columnValues(const SudokuCell& cell) {
+    std::set<int> values{};
+
+    for (int row = 0; row < 9; ++row) {
+        if (auto value = board[row * 9 + cell.column()])
+        {
+            values.insert(board[value]);
+        }
+    }
+    return values;
+}
+
+std::set<int> SudokuBoard::blockValues(const SudokuCell& cell) {
+    std::set<int> values;
+    int startRow = (cell.block() / 3) * 3;
+    int startColumn = (cell.block() % 3) * 3;
+    for (int row = startRow; row < startRow + 3; ++row)
+    {
+        for (int col = startColumn; col < startColumn + 3; ++col)
+        {
+            if (auto value = board[row * 9 + col])
+            {
+                values.insert(board[value]);
+            }
+        }
+    }
+    return values;
+}
+
+void SudokuBoard::updateCell(const SudokuCell& cell, const int& value) {
+    board[cell.arrayIndex] = value;
+}
--- SudokuBoard.hpp
+++ SudokuBoard.hpp
@@ -1,9 +1,40 @@
 #pragma once
 
 #include <array>
+#include <compare>
+#include <span>
+#include <set>
+
 
 constexpr int SUDOKU_CELL_COUNT = 81;
 using SudokuPuzzle = std::array<int, SUDOKU_CELL_COUNT>;
+
+struct SudokuCell {
+    explicit SudokuCell(int arrayIndex) noexcept
+    : arrayIndex(arrayIndex)
+    {}
+    
+    SudokuCell(const int& row, const int& col) noexcept 
+    : arrayIndex(row * 9 + col)
+    {}
+
+    // index value in array<int, 81>
+    int arrayIndex;
+
+    auto operator<=>(const SudokuCell&) const = default;
+
+    int row() const noexcept {
+        return arrayIndex / 9;
+    }
+
+    int column() const noexcept {
+        return arrayIndex % 9;
+    }
+
+    int block() const noexcept {
+        return (row() / 3) * 3 + (column() / 3);
+    }
+};
 
 class SudokuBoard {
 public:
@@ -14,6 +45,21 @@ public:
     SudokuBoard() = delete;
 
     void print();
+
+    void updateCell(const SudokuCell& cell, const int& value);
+
+    bool isEmpty(const SudokuCell& cell);
+
+    bool hasEmptyCells();
+    std::vector<SudokuCell> emptyCells();
+    std::vector<SudokuCell> emptyCellsInRow(const int& row);
+    std::vector<SudokuCell> emptyCellsInCol(const int& row);
+    std::vector<SudokuCell> emptyCellsInBlock(const int& block);
+
+    std::set<int> rowValues(const SudokuCell& cell);
+    std::set<int> columnValues(const SudokuCell& cell);
+    std::set<int> blockValues(const SudokuCell& cell);
+
 
 private:
     SudokuPuzzle board;
